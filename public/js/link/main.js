d5265b9d52c853290bb787e785d4904320807b0f
jambonbill <jambonbill@gmail.com>
2023-11-23 17:59:46 +0100

    function fetchPreview(){

--- public/js/link/main.js
+++ public/js/link/main.js
@@ -3,33 +3,41 @@
 // It would be nice to avoid fetching preview for links with a `Yazoo preview`
 
 let id=+document.getElementById("id").value;
-let el=document.getElementById('preview2');
-el.innerHTML="<i>fetching preview...</i>";
-
 let url=`/api/link/${id}/embed`
-console.log(`fetching link#${id} preview...`, url);
+let el=document.getElementById('preview2');
 
-fetch(url)
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-        return response.json();
-    })
-    .then(data => {
-        // Work with the JSON data
-        el.innerHTML='';
-        console.log(data); // Output the JSON data to the console
-        if(data.code){
-            console.log(data.code);
-            el.innerHTML=data.code;
-        }else{
-            console.log("No preview html code")
-        }
+if (el) {
+    el.innerHTML="<i>fetching preview...</i>";
+    console.log(`fetching link#${id} preview...`, url);
+    fetchPreview();
+}
 
-    })
-    .catch(error => {
-        console.error('There has been a problem with your fetch operation:', error);
-        el.innerHTML=error;
-    });
+function fetchPreview(){
+    
+    fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            // Work with the JSON data
+            el.innerHTML='';
+            //console.log(data);// Output the JSON data to the console
+            if (data.code) {
+                console.log(data.code);
+                el.innerHTML=data.code;
+            } else {
+                console.log("No preview html code")
+            }
+
+        })
+        .catch(error => {
+            console.error('There has been a problem with your fetch operation:', error);
+            el.innerHTML=error;
+        });
+}
+
+
 
