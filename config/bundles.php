34de862920e4e3087d9249903f82ddcb84e49f07
Tristan Rivoallan <tristan@rivoallan.net>
2023-12-03 08:59:35 +0100

    feat: Expose API documentation in OpenAPI format (#78)
    Fixes #74

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,5 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     SymfonyHealthCheckBundle\SymfonyHealthCheckBundle::class => ['all' => true],
+    Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
 ];
